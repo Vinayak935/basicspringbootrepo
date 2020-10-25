bc3ce19ba1b1df2d3b90ab513591fae808ea4f7a
Gireesh <girish@gmail.com>
2020-10-25 18:41:13 +0530

    main class modified

--- src/main/java/com/dsb/application/SpringbootdemoApplication.java
+++ src/main/java/com/dsb/application/SpringbootdemoApplication.java
@@ -2,12 +2,14 @@ package com.dsb.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringbootdemoApplication.class, args);
+		
 	}
 
 }
