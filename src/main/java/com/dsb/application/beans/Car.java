522615efc5eb14f709e41186b2c4683b21bc4578
Gireesh <girish@gmail.com>
2020-10-25 18:46:13 +0530

    new Car class added

---
+++ src/main/java/com/dsb/application/beans/Car.java
@@ -0,0 +1,12 @@
+package com.dsb.application.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+	public void start()
+	{
+		System.out.println("Car is Starting");
+	}
+}
